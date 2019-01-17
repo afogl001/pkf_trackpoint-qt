86310b2c1d6ac3176c948369a623aea99b662a86
afogl001 <afogl001@members.fsf.org>
2019-01-17 16:33:05 -0500

    Update with available code from previous repository

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,89 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-}
 
+    setSettingsPath();
+    validateSettings();
+    displaySettings();
+}
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_button_Apply_clicked()
+{
+    applySettings(settingPath + "/speed", ui->edit_Speed->text());
+    applySettings(settingPath + "/sensitivity", ui->edit_Sensativity->text());
+    applySettings(settingPath + "/press_to_select", ui->edit_Press_To_Select->text());
+
+    displaySettings();
+
+    ui->edit_Speed->clear();
+    ui->edit_Sensativity->clear();
+    ui->edit_Press_To_Select->clear();
+
+
+}
+
+void MainWindow::setSettingsPath()
+{
+    if (QFile::exists("/tmp/pkf_trackpoint/speed"))  // Make directory "/tmp/pkf_trackpoint" with "speed", "sensativity", and "press_to_select" if you'd like to test
+        settingPath = "/tmp/pkf_trackpoint";
+    else if (QFile::exists("/sys/devices/platform/i8042/serio1/serio2/speed"))  // If trackpad exists
+        settingPath = "/sys/devices/platform/i8042/serio1/serio2";
+    else if (QFile::exists("/sys/devices/platform/i8042/serio1/speed"))  // if trackpad does not exist
+        settingPath = "/sys/devices/platform/i8042/serio1";
+    else
+    {
+        QMessageBox::critical(this, "Alert", "No TrackPoint detected!");
+        exit(EXIT_FAILURE);
+    }
+
+    qDebug() << "Using trackpoint settings at " << settingPath;
+}
+
+void MainWindow::applySettings(QString settingFile, QString settingValue)
+{
+    QFile fileSet(settingFile);
+    if (settingValue!=NULL) {  // Only update setting if the lineedit is not NULL
+        if (fileSet.open(QIODevice::ReadWrite | QIODevice::Truncate)) {   // "QIODevice::Truncate" deletes the content of the file!
+            QTextStream stream(&fileSet);
+            stream << settingValue;
+        }
+        fileSet.close();
+    }
+}
+
+void MainWindow::validateSettings()
+{
+    QValidator *validateSpeedSensativity = new QIntValidator(1,255,this);
+    QValidator *validatePress_To_Select = new QIntValidator(0,1,this);
+    ui->edit_Speed->setValidator(validateSpeedSensativity);
+    ui->edit_Sensativity->setValidator(validateSpeedSensativity);
+    ui->edit_Press_To_Select->setValidator(validatePress_To_Select);
+}
+
+void MainWindow::displaySettings()
+{
+    QFile speedFile(settingPath + "/speed");
+    if(!speedFile.open(QIODevice::ReadOnly)) {
+        QMessageBox::information(0, "error", speedFile.errorString());
+    }
+    ui->label_Speed->setText(speedFile.readAll());
+    speedFile.close();
+
+    QFile sensativityFile(settingPath + "/sensitivity");
+    if(!sensativityFile.open(QIODevice::ReadOnly)) {
+        QMessageBox::information(0, "error", sensativityFile.errorString());
+    }
+    ui->label_Sensitivity->setText(sensativityFile.readAll());
+    sensativityFile.close();
+
+    QFile press_to_selectFile(settingPath + "/press_to_select");
+    if(!press_to_selectFile.open(QIODevice::ReadOnly)) {
+        QMessageBox::information(0, "error", press_to_selectFile.errorString());
+    }
+    ui->label_PressToSelect->setText(press_to_selectFile.readAll());
+    press_to_selectFile.close();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFile>
+#include <QIODevice>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QDebug>
+#include <QMessageBox>
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +20,21 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void setSettingsPath();
+    void applySettings(QString settingFile, QString settingValue);
+    void validateSettings();
+    void displaySettings();
+    void origionalFunction();
+
+private slots:
+    void on_button_Apply_clicked();
 
 private:
     Ui::MainWindow *ui;
+    QString settingPath;
+    QString speedPath;
+    QString sensativityPath;
+    QString press_to_selectPath;
 };
 
 #endif // MAINWINDOW_H
